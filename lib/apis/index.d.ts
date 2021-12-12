610dbd45ebadc205d31bb9d3a63c9227691d6adf
krakenon <luonguit@gmail.com>
2021-12-12 11:10:56 +0700

    expose routes v1.0.6

--- lib/apis/index.d.ts
+++ lib/apis/index.d.ts
@@ -1,2 +1,5 @@
+import { Router } from 'express';
+import { BaseApi } from './BaseApi';
 export * from './BaseApi';
+export declare function ConnectRoutes(router: Router, controllers: BaseApi[]): Router;
 //# sourceMappingURL=index.d.ts.map
--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,1 +1,12 @@
+import { Router } from 'express';
+import { BaseApi } from './BaseApi';
+
 export * from './BaseApi';
+
+export function ConnectRoutes(router: Router, controllers: BaseApi[]): Router {
+    controllers.forEach((c) => {
+        c.initRoutes();
+    });
+
+    return router;
+}
